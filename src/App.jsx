ed147dcb064d1130884c7f2fbad1fb41803a0cc4
Akash <akash906129@gmail.com>
2024-09-29 17:07:15 +0530

    updated Recoil Basics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,62 @@
-
-import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
-import { notifications, totalNotificationSelector } from './atoms'
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useRecoilValueLoadable,
+  useSetRecoilState,
+} from "recoil";
+import { notifications, totalNotificationSelector } from "./atoms";
+import { Suspense } from "react";
 
 function App() {
-  return <RecoilRoot>
-    <MainApp />
-  </RecoilRoot>
+  return (
+    <RecoilRoot>
+      <Suspense fallback={<>loading...</>}>
+        <MainApp />
+      </Suspense>
+    </RecoilRoot>
+  );
 }
 
-
-
-
 function MainApp() {
-  const [networkCount, setNetworkCount] = useRecoilState(notifications)
+  const networkCount = useRecoilValueLoadable(notifications);
   const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
   return (
-    <>
+    <div>
       <button>Home</button>
-      
-      <button>My network ({networkCount.networks >= 100 ? "99+" : networkCount.networks})</button>
+
+      <button>
+        My network (
+        {networkCount.networks >= 100 ? "99+" : networkCount.networks})
+      </button>
       <button>Jobs {networkCount.jobs}</button>
       <button>Messaging ({networkCount.messaging})</button>
       <button>Notifications ({networkCount.notifications})</button>
 
       <button>Me ({totalNotificationCount})</button>
-    </>
-  )
+    </div>
+  );
+
+  // if ((networkCount.state === "loading")) {
+  //   return <div>loading..</div>;
+  // } else if (networkCount.state === "hasValue") {
+  //   return (
+  //     <div>
+  //       <button>Home</button>
+
+  //       <button>
+  //         My network (
+  //         {networkCount.networks >= 100 ? "99+" : networkCount.networks})
+  //       </button>
+  //       <button>Jobs {networkCount.contents.jobs}</button>
+  //       <button>Messaging ({networkCount.contents.messaging})</button>
+  //       <button>Notifications ({networkCount.contents.notifications})</button>
+
+  //       <button>Me ({totalNotificationCount})</button>
+  //     </div>
+  //   );
+  // }
 }
 
-export default App
+export default App;
